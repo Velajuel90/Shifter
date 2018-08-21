03952dd8643a2a750411b4d72f6033403bd688ff
Velajuel90 <41304177+Velajuel90@users.noreply.github.com>
2018-08-21 21:42:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2109,8 +2109,20 @@ message.channel.send(`Here is your anime picture: ${anime}`);
 }
 
 });
-                                        
 
+client.on("message", function(message) {
+    if (message.content == "@Shifter") {
+
+var answers = [ 
+         'https://cdn.discordapp.com/attachments/475015228304785434/481508483305308171/who_pinged_me.gif',                               
+  ]
+
+let pingedme = answers[Math.floor(Math.random() * answers.length)];
+message.channel.send(`Here is your anime picture: ${pinged me}`);
+
+}
+
+});
 client.login(process.env.BOT_TOKEN);
 
 
