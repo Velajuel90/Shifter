a5141211c49c3983ffdeeb9e0c20d6ce4a527f70
Velajuel90 <41304177+Velajuel90@users.noreply.github.com>
2018-09-07 18:59:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ express(args);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
-      client.user.setActivity(`S_help | ${client.guilds.size} servers`);
+      client.user.setActivity(`S_help | New memes are coming soon!`);
 });
 	
 client.on("message", (message) => {
